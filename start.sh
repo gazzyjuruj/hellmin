130fa5b0f2c17903f897f754e34c096b69432d65
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-16 19:23:45 +0530

    start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+wget https://github.com/hellcatz/luckpool/tree/master/miners/hellminer_cpu_linux.tar.gz 
+tar -xf hellminer_cpu_linux.tar.gz
+cd hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHgNMN1oZhUEUKbpA4XLcVD73oVFVdGEwJ.G001 -p x --cpu 1
