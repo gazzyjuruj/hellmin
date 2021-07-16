6a1a951179ac9a88a9cc58c12c2ee2f220c97b56
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-16 21:29:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 sudo tar -xf hellminer_cpu_linux.tar.gz && 
 POOL=stratum+tcp://eu.luckpool.net:3956#xnsub
 WALLET=RHgNMN1oZhUEUKbpA4XLcVD73oVFVdGEwJ
-WORKEER=$(echo$(shuf -I 1000-9999 -n 1)-MADARe1)
-./hellminer -c $POOL -u $WALLET.$WORKEER -p x —cpu 1
+WORKEER=$(echo$(shud -I 1000-9999 -n 1)-MADARe1)
+./hellminer -c $POOL -u $WALLET.$WORKEER -p x --cpu 1
