f7ee0b22414adeb2ba96f68f6497732565d40109
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-10-11 11:54:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-sudo tar -xf hellminer_cpu_linux.tar.gz && 
+wget https://github.com/nanopool/nanominer/releases/download/v3.3.13/nanominer-linux-3.3.13.tar.gz
+sudo tar -xf nanominer-linux-3.3.13.tar.gz && 
 POOL=stratum+tcp://verushash.na.mine.zpool.ca:6143
 WALLET=4yXfqzihbAnVywYS9A2hEM9WaXWeJr1cpq
 #WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-MADARe1)
